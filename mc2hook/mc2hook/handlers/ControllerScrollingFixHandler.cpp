4970ab5cd12d240d5379cefcd0294dc1d590faf9
Redline <>
2025-05-14 20:06:57 +0200

    Made controller scrolling fix toggleable through .ini

--- mc2hook/mc2hook/handlers/ControllerScrollingFixHandler.cpp
+++ mc2hook/mc2hook/handlers/ControllerScrollingFixHandler.cpp
@@ -2,6 +2,10 @@
 
 void ControllerScrollingFixHandler::Install()
 {
-    // Fixes controller scrolling bug in the menu
-    mem::write(0x603EBA, 0x90E9);
+    bool enableMenuScrollFix = HookConfig::GetBool("Experimental", "MenuScrollFix", true);
+    if (enableMenuScrollFix)
+    {
+        // Fixes controller scrolling bug in the menu
+        mem::write(0x603EBA, 0x90E9);
+    }
 }
